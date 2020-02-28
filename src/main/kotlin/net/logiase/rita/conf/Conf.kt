faf63003df9c0e47e5be80e28c529208bae373a6
Logiase <logiase@outlook.com>
2020-02-28 23:43:52 +0800

    更新Readme

--- src/main/kotlin/net/logiase/rita/conf/Conf.kt
+++ src/main/kotlin/net/logiase/rita/conf/Conf.kt
@@ -4,13 +4,17 @@ import kotlinx.serialization.*
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonConfiguration
 import java.io.File
+import kotlin.system.exitProcess
 
 object Conf {
 
-    lateinit var conf: ConfData
 
+    var conf: ConfData
+
+    // 是否回应屏蔽
     private var ifResponse: Boolean = true
 
+    // 配置文件
     private val confFile = File("conf.json")
 
     init {
@@ -18,9 +22,15 @@ object Conf {
             conf = Json(JsonConfiguration.Stable).parse(ConfData.serializer(), confFile.readText())
         } else {
             print("conf.json Not Found")
+            exitProcess(-1)
         }
     }
 
+    /**
+     * 检查[id]再配置中的权限
+     * @param id
+     * @return 1-主人 0-可以回应 -1-拒绝回应
+     */
     fun checkPermission(id: Long): Int {
         if (id == conf.master) return 1
         if (ifResponse) return 0
@@ -28,6 +38,10 @@ object Conf {
         return 0
     }
 
+    /**
+     * 屏蔽[id]
+     * @param id 账号
+     */
     fun addBlock(id: Long) {
         if (id !in conf.blockAccounts) {
             conf.blockAccounts.add(id)
@@ -35,6 +49,10 @@ object Conf {
         }
     }
 
+    /**
+     * 解除[id]屏蔽
+     * @param id 账号
+     */
     fun removeBlock(id: Long) {
         if (id in conf.blockAccounts) {
             conf.blockAccounts.remove(id)
@@ -42,10 +60,17 @@ object Conf {
         }
     }
 
+    /**
+     * 保存配置
+     */
     private fun saveConf() {
         confFile.writeText(Json(JsonConfiguration.Stable).stringify(ConfData.serializer(), conf))
     }
 
+    /**
+     * 切换屏蔽状态
+     * @return 当前回应状态
+     */
     fun limit(): Boolean {
         ifResponse = !ifResponse
         return ifResponse
